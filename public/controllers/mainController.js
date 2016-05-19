c2533a2489b18078943c236a5f1242ecbc082fcc
Farid Hajnal <faridhajnal14@hotmail.com>
2016-05-19 21:57:56 +0200

    New visual functionalities

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -4,7 +4,7 @@ var weatherApp = angular.module("weatherApp");
 weatherApp.controller("mainController", ["$scope", "cityService", 
 function(scope,cityService){
     
-    
+    var inputbox = document.getElementById('text input');
     scope.city = cityService.city;
     
     scope.$watch('city', function(){
@@ -12,6 +12,14 @@ function(scope,cityService){
         cityService.city = scope.city; //modify in service, and now the other controller gets updated value
         
     })
+
+    scope.resetMethod = function(){
+
+    	scope.city = "";
+    	document.getElementById('city-input').focus();
+    	//console.log('Click on reset button ');
+
+    }
     
     //console.log('hello from main controller');
     
