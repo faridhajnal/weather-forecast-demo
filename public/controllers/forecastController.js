b9fe026a2a148d09635661c1f076c64304447b51
Farid Hajnal <faridhajnal14@hotmail.com>
2016-05-21 17:40:44 +0200

    Custom service for API Call

--- public/controllers/forecastController.js
+++ public/controllers/forecastController.js
@@ -2,23 +2,17 @@ var weatherApp = angular.module("weatherApp");
 
 //NO USAR PUTOS SERVICIOS EN CONTROLADORES!!!!!!!
 
-weatherApp.controller("forecastController", ["$scope", "$resource", "$routeParams", "cityService",
- function(scope, resource, routeParams, cityService){
+weatherApp.controller("forecastController", ["$scope","$routeParams", "cityService", "weatherService",
+ function(scope, routeParams, cityService, weatherService){
     
     scope.city = cityService.city;
 
     scope.days = routeParams.days || '2'; //default value for when there's no param on ur (normal /forecast)
 
-    scope.weatherAPI = 
-    resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=000983ad3a23d1c6c860d91c81abf4b2", 
-    	{ callback: "JSON_CALLBACK"},
-    	{get: { method: "JSONP"}});
-
-    scope.weatherResult = scope.weatherAPI.get({ q: scope.city, cnt : scope.days});
-
-    // q and cnt are parameteres for the api
-
+    
     //console.log('hello from forecast controller');
+
+    scope.weatherResult = weatherService.getWeather(scope.city,scope.days);
 
     console.log(scope.weatherResult);
 
--- public/services/weatherServices.js
+++ public/services/weatherServices.js
@@ -5,4 +5,29 @@ weatherApp.service('cityService', function(){
     this.city = "Mexico City, MX"; //default value.
     
     
-})
+});
+
+
+weatherApp.service('weatherService', ['$resource', function(resource){
+
+
+	this.getWeather=function(city,days){  //this is the srvice
+
+			var weatherAPI = 
+		    resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=000983ad3a23d1c6c860d91c81abf4b2", 
+		    	{ callback: "JSON_CALLBACK"},
+		    	{get: { method: "JSONP"}});
+
+		    return weatherAPI.get({ q: city, cnt : days});
+
+		    // q and cnt are parameteres for the api
+
+
+
+	}
+
+
+
+}]);
+
+//services can also be injected in custom services!!
